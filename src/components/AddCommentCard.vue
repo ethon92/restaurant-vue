d859c30ee4c5f2e66cbdf9f84d16bef133225e88
張曜安 <chang.yao.an@gmail.com>
2026-01-18 16:35:49 +0800

    新增AddCommentCard

---
+++ src/components/AddCommentCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { createComment } from '@/api/modules/feature';
+import { ref } from 'vue';
+
+const user_id = ref();
+const restaurant_id = ref();
+const comment_content = ref('');
+const rating = ref();
+
+// 測試的預設資料
+// userId.value = 2;
+// restaurantId.value = 'C3_371020000A_000334';
+
+const AddComment = async()=>{
+    const trimmedComment = comment_content.value.trim()
+
+    const comment_data = {
+        user_id :user_id.value,
+        restaurant_id :restaurant_id.value,
+        comment_content : trimmedComment,
+        rating : rating,value
+    }
+    try{
+        const result = await AddComment(comment_data)
+        comment_content.value=''
+        rating.value=''
+        console.log(result)   
+    }
+    catch(error){
+        console.log('msg:',error.response.comment_data)
+    }
+}
+</script>
+<template>
+
+
+</template>
+<style scoped>
+
+</style>
